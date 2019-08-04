ab1994ecb839ea1c1326d508daa970f4fe8a214e
Robin <robin.debock.y7189@student.hogent.be>
2019-08-04 14:23:29 +0200

    Bottom navigation with two main fragments

--- app/src/main/java/org/hogent/phonelibrary/MainActivity.kt
+++ app/src/main/java/org/hogent/phonelibrary/MainActivity.kt
@@ -1,37 +1,61 @@
 package org.hogent.phonelibrary
 
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import android.widget.TextView
+import android.util.Log
+import org.hogent.phonelibrary.fragments.FavoritesFragment
+import org.hogent.phonelibrary.fragments.SearchFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FavoritesFragment.OnFragmentInteractionListener, SearchFragment.OnFragmentInteractionListener {
 
-    private lateinit var textMessage: TextView
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                textMessage.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
+        val fragmentToSwitchTo: Fragment =
+            when (item.itemId) {
+                R.id.navigation_search -> {
+                    SearchFragment.newInstance()
+                }
+                R.id.navigation_favorites -> {
+                    FavoritesFragment.newInstance()
+                }
+                else ->
+                    return@OnNavigationItemSelectedListener false
             }
-            R.id.navigation_dashboard -> {
-                textMessage.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                textMessage.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
+
+        switchFragment(fragmentToSwitchTo)
+        return@OnNavigationItemSelectedListener true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        textMessage = findViewById(R.id.message)
+        //Configure navigation view. Set the listener.
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        //Set the org.hogent.phonelibrary.fragments.SearchFragment as the default fragment on startup.
+        switchFragment(SearchFragment.newInstance())
+    }
+
+    /**
+     * Helper function to show a new fragment on the screen.
+     *
+     * @param fragment The fragment to switch to.
+     */
+    private fun switchFragment(fragment: Fragment) {
+        //Write log.
+        Log.i("Switch fragment", "Switching to fragment ${fragment::class.simpleName}")
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
