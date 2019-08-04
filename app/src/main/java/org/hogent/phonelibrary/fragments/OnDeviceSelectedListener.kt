310ecda0a3f757c4eaf7c0049b9c7d0d0d9d16ff
Robin <robin.debock.y7189@student.hogent.be>
2019-08-04 15:30:25 +0200

    Cleaned up Favorites fragment. Added a seperate interface holding the functionaty for the parent activity

---
+++ app/src/main/java/org/hogent/phonelibrary/fragments/OnDeviceSelectedListener.kt
@@ -0,0 +1,10 @@
+package org.hogent.phonelibrary.fragments
+
+interface OnDeviceSelectedListener {
+    /**
+     * Functionality of an Activity which can handle the selection of a device.
+     * TODO include the activity as a parameter.
+     *
+     */
+    fun onDeviceSelection()
+}
