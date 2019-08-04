294540af01a805436a2df3555d9c15a40fec1a0d
Robin <robin.debock.y7189@student.hogent.be>
2019-08-04 10:52:19 +0200

    Setup: tested the documentation with Dokka by using some dummy comments in Main Activity

--- app/src/main/java/org/hogent/phonelibrary/MainActivity.kt
+++ app/src/main/java/org/hogent/phonelibrary/MainActivity.kt
@@ -5,6 +5,11 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * Test documentation for the onCreate.
+     *
+     * @param savedInstanceState The saved instance state
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
